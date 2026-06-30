1f41a596e16d44cb675d07cc3d348698b1892fe8
David Shriver <davidshriver@outlook.com>
2026-06-30 17:37:01 -0400

    update install scripts

--- install_tool.sh
+++ install_tool.sh
@@ -5,9 +5,4 @@ if [ -d "~/.cache/veridnn/$name/$version" ]; then
     rm -r ~/.cache/veridnn/$name/$version
 fi
 
-curl -L -O https://github.com/veridnn/agate-vnncomp2026/releases/download/2026.06.30/agate-vnnlib.0.0.1.tar.gz.gpg_part_0
-curl -L -O https://github.com/veridnn/agate-vnncomp2026/releases/download/2026.06.30/agate-vnnlib.0.0.1.tar.gz.gpg_part_1
-curl -L -O https://github.com/veridnn/agate-vnncomp2026/releases/download/2026.06.30/agate-vnnlib.0.0.1.tar.gz.gpg_part_2
-curl -L -O https://github.com/veridnn/agate-vnncomp2026/releases/download/2026.06.30/agate-vnnlib.0.0.1.tar.gz.gpg_part_3
-
-cat agate-vnnlib.0.0.1.tar.gz.gpg_part_* > agate-vnnlib.0.0.1.tar.gz.gpg
+curl -L -O https://github.com/veridnn/agate-vnncomp2026/releases/download/0.0.1a1.dev1/agate-vnnlib.0.0.1.tar.gz.gpg
--- post_install.sh
+++ post_install.sh
@@ -2,4 +2,7 @@
 set -e
 
 gpg --batch --yes --passphrase "$1" -d agate-vnnlib.0.0.1.tar.gz.gpg | tar xz
-cp agate-vnnlib.0.0.1/agate .
+python3 -m venv .venv
+.venv/bin/pip install --upgrade pip
+cd agate-vnnlib.0.0.1
+../.venv/bin/pip install .
