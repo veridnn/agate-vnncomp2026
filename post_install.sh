a7d1624990e1d10b3b29b44227771707d9dcfbfd
David Shriver <davidshriver@outlook.com>
2026-06-30 18:51:16 -0400

    quick patch to make it build

--- post_install.sh
+++ post_install.sh
@@ -5,4 +5,4 @@ gpg --batch --yes --passphrase "$1" -d agate-vnnlib.0.0.1.tar.gz.gpg | tar xz
 python3.14 -m venv .venv
 .venv/bin/pip install --upgrade pip
 cd agate-vnnlib.0.0.1
-../.venv/bin/pip install .
+../.venv/bin/pip install -e .
