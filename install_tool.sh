2aac1d7b7481c0758fd6f3f1b9752843526953f8
David Shriver <davidshriver@outlook.com>
2026-06-30 18:16:11 -0400

    use more recent python

--- install_tool.sh
+++ install_tool.sh
@@ -5,4 +5,8 @@ if [ -d "~/.cache/veridnn/$name/$version" ]; then
     rm -r ~/.cache/veridnn/$name/$version
 fi
 
+sudo add-apt-repository ppa:deadsnakes/ppa
+sudo apt update
+sudo apt install python3.14 python3.14-venv python3.14-dev
+
 curl -L -O https://github.com/veridnn/agate-vnncomp2026/releases/download/0.0.1a1.dev1/agate-vnnlib.0.0.1.tar.gz.gpg
