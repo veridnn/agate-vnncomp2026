a26597731704f57a039bdd9e810150185bf18527
David Shriver <davidshriver@outlook.com>
2026-06-30 21:00:14 -0400

    install cuda versions of dependencies

--- post_install.sh
+++ post_install.sh
@@ -5,4 +5,5 @@ gpg --batch --yes --passphrase "$1" -d agate-vnnlib.0.0.1.tar.gz.gpg | tar xz
 python3.14 -m venv $HOME/.venv
 $HOME/.venv/bin/pip install --upgrade pip
 cd agate-vnnlib.0.0.1
-$HOME/.venv/bin/pip install -e .
+$HOME/.venv/bin/pip install -e ".[cuda13]"
+$HOME/.venv/bin/agate --version
